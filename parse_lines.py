5b798f6f5668a54a815ea439b3d2e6d23306daa7
Tony Blatt <tonyblatt@gmail.com>
2023-01-15 14:36:16 -0500

    Change to how python script exits.

--- parse_lines.py
+++ parse_lines.py
@@ -2,8 +2,14 @@
 
 import sys
 
+exit_success = True
+
 for i in range(1, len(sys.argv)):
 	line = sys.argv[i]
 	if "==" != line[0:2]:
+		exit_success = False
 		parsed_line = "./" + line.replace("-=-", " ")
 		sys.stdout.write(parsed_line)
+
+if not exit_success:
+	sys.exit(1)
