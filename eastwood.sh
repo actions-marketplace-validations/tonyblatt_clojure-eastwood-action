e0b9462d975ff21dcf6fe4bf0cac80d50720c138
Tony Blatt <tonyblatt@gmail.com>
2023-01-15 15:13:05 -0500

    Change how shell and python scripts work.

--- eastwood.sh
+++ eastwood.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
-
 cd "${GITHUB_WORKSPACE}" || exit 1
 
 git config --global --add safe.directory "$GITHUB_WORKSPACE" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-lein eastwood "${INPUT_EASTWOOD_FLAGS}" \
-  | sed -e "s/ /-=-/g" \
-  | python3 parse_lines.py \
+eastwood_output="$(lein eastwood ${INPUT_EASTWOOD_FLAGS} | sed -e "s/ /-=-/g")"
+
+python3 parse_lines.py $eastwood_output \
   | reviewdog \
       -efm="%f:%l:%c: %m" \
-      -name="clj-kondo" \
+      -name="clj-eastwood" \
       -reporter="${INPUT_REPORTER}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
--- parse_lines.py
+++ parse_lines.py
@@ -6,9 +6,9 @@ exit_success = True
 
 for i in range(1, len(sys.argv)):
 	line = sys.argv[i]
-	if "==" != line[0:2]:
+	if "==" != line[0:2] and "Directories" != line[0:len("Directories")] and "Subprocess" != line[0:len("Subprocess")]:
 		exit_success = False
-		parsed_line = "./" + line.replace("-=-", " ")
+		parsed_line = "./" + line.replace("-=-", " ") + "\n"
 		sys.stdout.write(parsed_line)
 
 if not exit_success:
